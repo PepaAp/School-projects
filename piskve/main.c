85e3fbe3b65d54aaea45fe7e0866f31eecd4f9b1
Josef Aron <144839725+PepaAp@users.noreply.github.com>
2024-12-11 22:15:21 +0100

    Diagonal win added

--- piskve/main.c
+++ piskve/main.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
-#define SIZE 3
-#define WIN 2
+#define SIZE 10
+#define WIN 3
 
 void printTable (char table[SIZE][SIZE]) {
     printf("Piskvorky\n\n");
     printf("   ");
-    for (int k = 0; k < SIZE; k++) {
+    for (int k = 0; k < SIZE; k++) {            // vypise cislo sloupce
         printf("|%4.2d ", k + 1);
     }
     printf("\n");
 
-    for (int i = 0; i < SIZE; i++) {
-        printf("|%.2d", i + 1);
-        for (int j = 0; j < SIZE; j++) {
+    for (int i = 0; i < SIZE; i++) {           // vypise hraci pole
+        printf("|%.2d", i + 1);          // vypise cislo radku
+        for (int j = 0; j < SIZE; j++) {                // vypise obsah v policku
             printf("| [%c] ", table[i][j]);
         }
         printf("\n");
@@ -22,36 +21,39 @@ void printTable (char table[SIZE][SIZE]) {
 
 void turn (char table[SIZE][SIZE], char player) {
     int x, y;
-    printf("Players %c turn, choose your tile (row and column): ", player);
+    int countChar = 0;
+    printf("Players %c turn, choose your tile (row space column): ", player);       //prvni hrac si vzbere pole a ulozi se docasne jeho tah
     scanf("%d %d", &x, &y);
+    while (getchar() != '\n') {
+        countChar++;
+    };
     x--;
     y--;
-
-    if (table[x][y] == ' ') {
-        table[x][y] = player;
-    } else {
-        printf("Invalid move, try again.\n");
+    if (countChar != 0 || x<0 || x>SIZE-1 || y<0|| y>SIZE-1) {              //kontroluje zda je vyber pole validni
+        printf("Invalid input, try again.\n");
         turn(table, player);
+    } else {
+        table[x][y] = player;               //yapise tah do pole
     }
 }
 
 int column (char table[SIZE][SIZE]) {
     int countX, countO;
-    for (int i = 0; i < SIZE; i++) {
+    for (int i = 0; i < SIZE; i++) {        //meni sloupce
         countX = 0;
         countO = 0;
-        for (int j = 0; j < SIZE; j++) {
-            if (table[j][i] == 'x') {
+        for (int j = 0; j < SIZE; j++) {   //meni radky
+            if (table[j][i] == 'x') {       //pokud je x zvysi pocet x a vznuluje pocet o
                 countO = 0;
                 countX++;
-            } else if (table[j][i] == 'o') {
+            } else if (table[j][i] == 'o') { //pokud je o zvysi pocet o a vznuluje pocet x
                 countX = 0;
                 countO++;
-            } else {
+            } else {                               // pokud je prazdne vynuluje oba
                 countO = 0;
                 countX = 0;
             }
-            if (countX == WIN || countO == WIN) {
+            if (countX == WIN || countO == WIN) {       //pokud je pocet x nebo y roven nastavenemu poctu pro vyhru ukonci hru vypise pole a viteze
                 printTable(table);
                 printf("%c wins", (countX == WIN) ? 'x' : 'o');
                 return 1;
@@ -63,11 +65,11 @@ int column (char table[SIZE][SIZE]) {
 
 int row (char table[SIZE][SIZE]) {
     int countX, countO;
-    for (int i = 0; i < SIZE; i++) {
+    for (int i = 0; i < SIZE; i++) {            //meni radky
         countX = 0;
         countO = 0;
-        for (int j = 0; j < SIZE; j++) {
-            if (table[i][j] == 'x') {
+        for (int j = 0; j < SIZE; j++) {        //meni sloupce
+            if (table[i][j] == 'x') {           //to same jako predtim jen pro radky
                 countO = 0;
                 countX++;
             } else if (table[i][j] == 'o') {
@@ -89,7 +91,53 @@ int row (char table[SIZE][SIZE]) {
 
 int diagonal (char table[SIZE][SIZE]) {
     int countX, countO;
-    
+    for (int i = 0; i < SIZE; i++) {            //meni radky
+        countX = 0;
+        countO = 0;
+        for (int j = 0; j < SIZE; j++) {        //meni sloupce
+            if (table [j][i] != ' ') {
+                if (table[j][i] == 'x') {
+                    countO = 0;
+                    countX++;
+                    for (int k = 1; k < WIN; k++) {
+                        if (table[j + k][i + k] == 'x') {
+                            countX++;
+                            if (countX == WIN) {
+                                printTable(table);
+                                printf("x wins");
+                                return 1;
+                            }
+                        } else if (table[j + k][i - k] == 'x') {
+                            countX++;
+                            if (countX == WIN) {
+                                printTable(table);
+                                printf("x wins");
+                                return 1;
+                            }
+                        } else {
+                            countX = 0;
+                        }
+                    }
+                } else if (table[j][i] == 'o') {
+                    countX = 0;
+                    countO++;
+                    for (int k = 1; k < WIN; k++) {
+                        if (table[j + k][i + k] == 'o') {
+                            countO++;
+                            if (countO == WIN) {
+                                printTable(table);
+                                printf("o wins");
+                                return 1;
+                            }
+                        } else {
+                            countO = 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     return 0;
 }
 
