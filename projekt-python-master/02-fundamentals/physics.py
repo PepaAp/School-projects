c1276720f1cf0d6ace623aac5bef0155a01391c6
Josef Aron <144839725+PepaAp@users.noreply.github.com>
2025-10-20 17:31:04 +0200

    Update physics.py

--- projekt-python-master/02-fundamentals/physics.py
+++ projekt-python-master/02-fundamentals/physics.py
@@ -6,11 +6,10 @@ V Pythonu jsou konstanty obvykle deklarovány a přiřazovány v modulu, který 
 Konstanty jsou pojmenovány velkými písmeny a jednotlivá slova jsou oddělována podtržítky.
 '''
 
-EARTH_GRAVITY = 0 #? normální pozemské tíhové zrychlení
-MOON_GRAVITY = 0 #? měsíční gravitace
-SPEED_OF_LIGHT = 0 #? rychlost světla ve vakuu
-SPEED_OF_SOUND = 0 #? rychlost zvuku při teplotě 20 °C v suchém vzduchu
-
+EARTH_GRAVITY = 9.807 #ms
+MOON_GRAVITY = 1.622 #ms
+SPEED_OF_LIGHT = 299792458 #ms
+SPEED_OF_SOUND = 343 #ms
 ''' 
 Úkol:
 1. Doplňte správně hodnoty uvedených konstant.
@@ -20,3 +19,28 @@ Samozřejmě můžete své řešení rozšířit i o jiné fyzikální konstanty
 4. Vytvořte vlastní aplikaci myapp.py, do níž tento modul importujte. Demonstrujte v ní na jednoduchých příkladech využití vámi
 připravených funkcí.  
 '''
+
+def weightOnMoon(w):
+    """calculates your weight if you lived on the MOON.
+    Input in Kg.
+    Output in Kg."""
+    return w/EARTH_GRAVITY*MOON_GRAVITY
+
+def storm(dif) :
+    """calculates how far is the storm based on the delay between light of sound of lighting. 
+    Input in seconds.
+    Output in meters."""
+    return int(SPEED_OF_SOUND * dif)
+
+def daBOOM(far):
+    """Based on how far the explosion happened, calculates when the sound wave will hit.
+    Input distance in Km.
+    Output in seconds.
+    """
+    return far*1000 / SPEED_OF_SOUND
+
+def lightYearDistance ():
+    """calculates how manz km does light past in 1 year.
+    No input.
+    Output in Km."""
+    return int((SPEED_OF_LIGHT * 365 * 24 * 3600) / 1000)
