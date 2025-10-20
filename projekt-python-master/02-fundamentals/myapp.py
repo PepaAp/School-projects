179596e8d669665ecb75754047a2fd702ef3a010
Josef Aron <144839725+PepaAp@users.noreply.github.com>
2025-10-20 17:31:34 +0200

    Add files via upload

---
+++ projekt-python-master/02-fundamentals/myapp.py
@@ -0,0 +1,6 @@
+import physics
+
+print(f"you would weigh {physics.weightOnMoon(80):.2f} kg on the moon")
+print(f"the explosion's sound wave will hit in {physics.daBOOM(40):.2f} seconds")
+print(f"the light travels {physics.lightYearDistance()} km in a year")
+print(f"the storm is {physics.storm(5):.2f} m far away")
